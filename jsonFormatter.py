253b84e011df647a2167d07222fae2ba7aceea2f
Adamandom <74009107+Adamandom@users.noreply.github.com>
2021-08-29 22:10:02 +0100

    tweaks
    index.html didnt update??

--- jsonFormatter.py
+++ jsonFormatter.py
@@ -1,4 +1,13 @@
+# this script takes simply reads the data from the export page and answer tsv files
+# and converts them to JSON which is then read by the VGMB. 
 
+# To avoid the stress of asynchronous malarkey, its simply pasted into the ALLSONGS.js as a string.
+# Not great practice, but it works and saves me the headache.
+
+# The export page and answer TSV files are gotten from the VGMB spreadsheet.
+# TSV was chosen as game titles can have commas.
+
+# dont ask why this is OO and not functional. I dont know either.
 class VGMBSongFormatter:
     def __init__(this, file, ansFile):
         this.VGMB_TSV_file = file
@@ -8,9 +17,10 @@ class VGMBSongFormatter:
         this.allSongsArr = []
         # [ ID, Title, Game, Series, Link, Difficulty ]
         this.answers = []
-        # [ ID, Game, Series, Answer0, ... AnswerX ]
+        # [ ID, Game, Series, Answer0, ... AnswerN ]
 
 
+    # converts the TSV file to a workable ar- I mean list. Sorry python has to be special.
     def formatTSVAsArr(this):
         for line in this.VGMB_TSV_file:
             line = line.strip()
@@ -37,7 +47,6 @@ class VGMBSongFormatter:
             if i > 0:
                 json += "},"
 
-            # new series
             # new game in the same series
             if this.allSongsArr[i][2] != currentGame and this.allSongsArr[i][3] == currentSeries:
                 isNewSeries = False
@@ -45,6 +54,7 @@ class VGMBSongFormatter:
                 currentGame = this.allSongsArr[i][2]
                 json += this.formatNewGame(i, isNewSeries)
 
+            # new series
             if this.allSongsArr[i][3] != currentSeries:
                 isNewSeries = True
                 json = json[:len(json) - 1]
@@ -92,22 +102,28 @@ class VGMBSongFormatter:
         output = ""
         for i in range( len(this.answers[index]) ):
 
+            # filters out the game ID (0th pos) and series (2th pos) 
             if ( i == 0 or i == 2  ):
                 continue
+
             output += "\"" + this.answers[index][i].lower() + "\""
 
+            # if there are no answers beyond the title (1th pos)
+            # due to lack of permuation/ simplification. eg: Ballon Fight
             if len( this.answers[index] ) == 3:
                 continue
 
+            # Condition prevents trailing commas
             if i != len(this.answers[index]) - 1:
                 output += ", "
 
         return output
 
+    # sloppy, but this just closes off the json.
     def formatEndOfArr(this):
         return "} } } } } } }"
 
-
+    # essentially the master function of the object
     def beginFormatting(this):
         json = "{ \"series\": { "
         json += this.formatNewSeries(0)
@@ -120,7 +136,7 @@ class VGMBSongFormatter:
 # # # # #
 # main
 # # # # #
-
+# init the formatter, format the files as lists and generate + write the json to file.
 formatter = VGMBSongFormatter( open( "VGMB - Export Page.tsv", "r" ), open( "VGMB - Answers.tsv", "r" ) )
 formatter.formatTSVAsArr()
 
